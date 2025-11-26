501a0b72c24bd34e16791b1f09813da6270de311
LAPTOP-2LL78EN6\nwl <panyachalee.c@psd.co.th>
2025-11-26 11:13:11 +0700

    add section for tester to test without redirect token

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -4,6 +4,11 @@ import React from "react";
 export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
   const token = localStorage.getItem("auth_token");
 
+    // allow access in test/preview environment
+  const isPreview = import.meta.env.VITE_APP_IS_PREVIEW === "true";
+
+  if (isPreview) return children;
+
   if (!token) {
     return <Navigate to="/404" replace />;
   }
