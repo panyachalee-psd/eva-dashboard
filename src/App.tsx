cc6eb95547fddccf3ba05a669203e4bf153f2c40
LAPTOP-2LL78EN6\nwl <panyachalee.c@psd.co.th>
2025-11-26 14:14:57 +0700

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import VerifyToken from "@/pages/Verify";
 import DashboardLayout from "@/components/DashboardLayout";
 import NotFound from "@/components/Notfound";
@@ -6,8 +6,10 @@ import ProtectedRoute from "@/components/ProtectedRoute";
 
 export default function App() {
   return (
-    
     <Routes>
+      {/* root redirect */}
+      <Route path="/" element={<Navigate to="/dashboard" replace />} />
+
       {/* token verification */}
       <Route path="/verify" element={<VerifyToken />} />
 
@@ -21,15 +23,8 @@ export default function App() {
         }
       />
 
-      {/* pretty 404 */}
       <Route path="/404" element={<NotFound />} />
-
-      {/* fallback for everything else */}
       <Route path="*" element={<NotFound />} />
-
-       {/* <div>
-      Preview mode: {import.meta.env.VITE_APP_IS_PREVIEW || "undefined"}
-    </div> */}
     </Routes>
   );
 }
