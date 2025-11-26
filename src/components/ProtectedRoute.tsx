37f82e6b92357d9e16be3da650bd4a970a9d4131
LAPTOP-2LL78EN6\nwl <panyachalee.c@psd.co.th>
2025-11-26 13:34:03 +0700

    back to oriptroute

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,40 +1,18 @@
-import React from "react";
 import { Navigate } from "react-router-dom";
-
-// interface ProtectedRouteProps {
-//   children: React.ReactNode;
-// }
+import React from "react";
 
 export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
   const token = sessionStorage.getItem("auth_token");
 
-  const isPreview =
-    import.meta.env.VITE_APP_IS_PREVIEW === "true" ||
-    window.location.hostname.includes("vercel.app");
+    // allow access in test/preview environment
+  const isPreview = import.meta.env.VITE_APP_IS_PREVIEW === "true";
+  console.log("Preview mode:", import.meta.env.VITE_APP_IS_PREVIEW);
 
-  console.log("ProtectedRoute Preview mode:", isPreview);
-  console.log("Token:", token);
+  if (isPreview) return children;
 
-  if (isPreview) return <>{children}</>;
-  if (!token) return <Navigate to="/404" replace />;
+  if (!token) {
+    return <Navigate to="/404" replace />;
+  }
+
   return <>{children}</>;
 }
-
-// import { Navigate } from "react-router-dom";
-// import React from "react";
-
-// export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
-//   const token = sessionStorage.getItem("auth_token");
-
-//     // allow access in test/preview environment
-//   const isPreview = import.meta.env.VITE_APP_IS_PREVIEW === "true";
-//   console.log("Preview mode:", import.meta.env.VITE_APP_IS_PREVIEW);
-
-//   if (isPreview) return children;
-
-//   if (!token) {
-//     return <Navigate to="/404" replace />;
-//   }
-
-//   return <>{children}</>;
-// }
